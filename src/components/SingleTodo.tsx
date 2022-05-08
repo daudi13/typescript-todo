1987799a1f70c13422f6d495364b5bf2c1a2054b
daudi13 <davouma13@gmail.com>
2022-05-08 13:31:07 +0300

    Add singletodo component

---
+++ src/components/SingleTodo.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface Props {
+  todo: string;
+}
+
+const SingleTodo: React.FC<Props> = ({todo}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SingleTodo
